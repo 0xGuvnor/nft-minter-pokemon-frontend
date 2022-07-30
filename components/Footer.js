06c9e1adcc277e738bdef7db3120b03e840078ca
0xbagholder <101540063+0xbagholder@users.noreply.github.com>
2022-07-30 22:58:43 +0800

    Update Github username

--- components/Footer.js
+++ components/Footer.js
@@ -60,7 +60,7 @@ const Footer = () => {
                         Get free testnet Ether
                     </a>
                     <a href={githubRepo} target="_blank" rel="noopener noreferrer">
-                        Built by 0xbagholder
+                        Built by 0xpaprHands
                     </a>
                 </div>
                 <div className={style.rightDetails}>Pokedéx Labs © 2022 </div>
