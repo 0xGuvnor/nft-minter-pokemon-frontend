85ea9e6aa442e23f110b267ad0d6f1693c946ec7
0xpaprhnds <101540063+0xpaprHands@users.noreply.github.com>
2022-07-31 16:13:33 +0800

    Update Username

--- components/Footer.js
+++ components/Footer.js
@@ -60,7 +60,7 @@ const Footer = () => {
                         Get free testnet Ether
                     </a>
                     <a href={githubRepo} target="_blank" rel="noopener noreferrer">
-                        Built by 0xpaprHands
+                        Built by 0xpaprhnds
                     </a>
                 </div>
                 <div className={style.rightDetails}>Pokedéx Labs © 2022 </div>
--- constants/urlLinks.js
+++ constants/urlLinks.js
@@ -4,5 +4,5 @@ export const twitterUrl = "https://twitter.com/";
 export const discordUrl = "https://discord.com/";
 export const telegramUrl = "https://web.telegram.org/k/";
 export const githubUrl = "https://github.com/";
-export const githubRepo = "https://github.com/0xpaprHands";
+export const githubRepo = "https://github.com/0xpaprhnds";
 export const etherscanUrl = "https://rinkeby.etherscan.io/";
