89f8d3353229b3f9185749e14c81c68ba208f50e
0xpaprhnds <yokeyeong@me.com>
2023-03-09 17:35:09 +0100

    Update urlLinks.js

--- constants/urlLinks.js
+++ constants/urlLinks.js
@@ -5,4 +5,4 @@ export const discordUrl = "https://discord.com/";
 export const telegramUrl = "https://web.telegram.org/k/";
 export const githubUrl = "https://github.com/";
 export const githubRepo = "https://github.com/0xpaprhnds";
-export const etherscanUrl = "https://rinkeby.etherscan.io/";
+export const etherscanUrl = "https://goerli.etherscan.io/";
